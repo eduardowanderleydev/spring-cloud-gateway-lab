4d9133a22ccaa08f9ecb5687c005db94da12deec
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2024-11-12 08:57:18 -0300

    Creates an ordinary service

---
+++ ordinary-service/src/main/java/br/wanderley/com/ordinaryservice/OrdinaryServiceApplication.java
@@ -0,0 +1,13 @@
+package br.wanderley.com.ordinaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdinaryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdinaryServiceApplication.class, args);
+    }
+
+}
