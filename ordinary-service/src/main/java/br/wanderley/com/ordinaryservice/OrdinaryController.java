16d4b7af429410a46eba7118aff522ae0fb9c140
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2024-11-12 14:13:39 -0300

    Create gateway configurations

--- ordinary-service/src/main/java/br/wanderley/com/ordinaryservice/OrdinaryController.java
+++ ordinary-service/src/main/java/br/wanderley/com/ordinaryservice/OrdinaryController.java
@@ -1,9 +1,11 @@
 package br.wanderley.com.ordinaryservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/greetings")
 public class OrdinaryController {
 
     @GetMapping("/hello")
